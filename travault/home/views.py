ba55e9ad93001e78755f0548b329b5c0a56b1252
Tyrone Francis <tyrone.h.francis@gmail.com>
2024-08-26 16:29:15 +0000

    trying to add regiser.html

--- travault/home/views.py
+++ travault/home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -12,3 +15,16 @@ def terms_of_service(request):
 def privacy_policy(request):
     """ A view to return the Privacy Policy page. """
     return render(request, 'privacy_policy.html')
+
+# def register(request):
+#     print("Register view called")
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             messages.success(request, f"Welcome to TraVault, {user.username}!")
+#             return redirect('dashboard') 
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'register.html', {'form': form})
