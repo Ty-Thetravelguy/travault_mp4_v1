2c401f6a4fb37d33d722d75112b48abcd9832b5f
Tyrone Francis <tyrone.h.francis@gmail.com>
2024-08-26 14:03:32 +0000

    Added terms of servcie, and privay policy pages

--- travault/home/views.py
+++ travault/home/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page. """
     return render (request, 'home/index.html')
+
+def terms_of_service(request):
+    """ A view to return the Terms of Service page. """
+    return render(request, 'terms_of_service.html')
+
+def privacy_policy(request):
+    """ A view to return the Privacy Policy page. """
+    return render(request, 'privacy_policy.html')
